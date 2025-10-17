83906e5acf591b2df28552ff1fedc17053c63de5
JOHNROSSDOHERTY-STUD <dohertyjr0@icloud.com>
2025-10-17 10:26:37 +0100

    added PUT to controller

---
+++ src/main/java/ie/atu/passengerservice/PassengerServiceApplication.java
@@ -0,0 +1,10 @@
+package ie.atu.passengerservice;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassengerServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PassengerServiceApplication.class, args);
+    }
+}
