02c1f5733c81a73f3678c1a64dd3dc5f65e980cc
JOHNROSSDOHERTY-STUD <dohertyjr0@icloud.com>
2025-10-17 09:47:37 +0100

    added testing for get and post and maven-ci file

--- src/test/java/ie/atu/passengerservice/PassengerServiceApplicationTests.java
+++ src/test/java/ie/atu/passengerservice/PassengerServiceApplicationTests.java
@@ -1,13 +1,53 @@
 package ie.atu.passengerservice;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 class PassengerServiceApplicationTests {
+    private PassengerServiceApplication service;
 
-    @Test
-    void contextLoads() {
+    @BeforeEach
+    void setUp() {
+        service = new PassengerServiceApplication();
     }
 
+    @Test
+    void createThenFindById() {
+        Passenger p = Passenger.builder()
+                .passengerId("P1")
+                .name("Paul")
+                .email("paul@atu.ie")
+                .build();
+
+        service.create(p);
+
+        Optional<Passenger> found = service.findById("P1");
+        assertTrue(found.isPresent());
+        assertEquals("Paul", found.get().getName());
+    }
+
+    @Test
+    void duplicateIdThrows(){
+        service.create(Passenger.builder()
+                .passengerId("P2")
+                .name("Bob")
+                .email("bob@ex.com")
+                .build()
+        );
+
+        assertThrows(IllegalArgumentException.class, () ->
+                service.create(Passenger.builder()
+                        .passengerId("P2")
+                        .name("Bobby")
+                        .email("bob@ex.com")
+                        .build()
+                )
+                );
+    }
 }
